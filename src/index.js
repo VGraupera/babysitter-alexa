877c7a7714727dc603bd4b5e27588a30835397b3
Vidal Graupera <vgraupera@gmail.com>
2017-03-28 12:37:54 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var Alexa = require('alexa-sdk');
+var APP_ID = undefined; // TODO replace with your app ID (OPTIONAL).
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    //Use LaunchRequest, instead of NewSession if you want to use the one-shot model
+    // Alexa, ask [my-skill-invocation-name] to (do something)...
+    'LaunchRequest': function () {
+        this.attributes['speechOutput'] = this.t("WELCOME_MESSAGE", this.t("SKILL_NAME"));
+        // If the user either does not reply to the welcome message or says something that is not
+        // understood, they will be prompted again with this text.
+        this.attributes['repromptSpeech'] = this.t("WELCOME_REPROMPT");
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.HelpIntent': function () {
+        this.attributes['speechOutput'] = this.t("HELP_MESSAGE");
+        this.attributes['repromptSpeech'] = this.t("HELP_REPROMPT");
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'SessionEndedRequest':function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    'Unhandled': function () {
+        this.attributes['speechOutput'] = this.t("HELP_MESSAGE");
+        this.attributes['repromptSpeech'] = this.t("HELP_REPROMPT");
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    }
+};
+
+var languageStrings = {
+    "en": {
+        "translation": {
+            "SKILL_NAME": "Babysitter",
+            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, what\'s mom\'s phone number? or what\'s dad\'s phone? ... Now, what can I help you with.",
+            "WELCOME_REPROMPT": "For instructions on what you can say, please say help me.",
+            "HELP_MESSAGE": "You can ask questions such as, what\'s mom\'s phone number, or, you can say exit...Now, what can I help you with?",
+            "HELP_REPROMPT": "You can say things like, what\'s mom\'s phone number, or you can say exit...Now, what can I help you with?",
+            "STOP_MESSAGE": "Goodbye!",
+        }
+    }
+};
